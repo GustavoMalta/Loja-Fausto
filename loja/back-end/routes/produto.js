07ed0e6e5b4b31116a4ab64e92058f94c7f7c1e7
GustavoMalta <gustavo.a.malta@gmail.com>
2018-05-03 13:46:17 -0300

    Mudança diretorios

--- loja/back-end/routes/produto.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/produto')(/* construtor */);
-
-router.get('/', controller.listar);
-router.get('/:id', controller.obterUm);
-
-module.exports = router;
